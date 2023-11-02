f185fd3d239b24095dfaaf626348f8a8a54ac6ac
sbx_user1051 <sbx_user1051@169.254.189.89>
2023-11-02 16:10:22 +0000

    Thu Nov 02 16:10:22 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -11,14 +11,14 @@ class TestCheckout:
     def test_checkout_5A_deal(self):
         assert checkout_solution.checkout('ABCDAAAACA') == 500+30+20+15+20+50
 
-    def test_checkout_both_deal(self):
-        assert checkout_solution.checkout('ABCDAAAACAAA') == 500+130+20+15+20
+    def test_checkout_both_A_deal(self):
+        assert checkout_solution.checkout('ABCDAAAACAAA') == 500+130+30+20+15+20
     
     def test_checkout_B_deal(self):
         assert checkout_solution.checkout('ABCBBBD') == 45+45+50+20+15
     
     def test_checkout_E_deal(self):
-        assert checkout_solution.checkout('ABCDEE') == 50+20+15+40+40
+        assert checkout_solution.checkout('ABCDEE') == 50+30+20+15+40+40
     
     def test_checkout_B_and_E_deals(self):
         assert checkout_solution.checkout('ABCDBEE') == 50+45+20+15+15
