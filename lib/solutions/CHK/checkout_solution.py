fa3d67c6dbafab20cdf2c8ca0ab0f07bb2edf692
sbx_user1051 <sbx_user1051@169.254.189.89>
2023-11-02 16:22:22 +0000

    Thu Nov 02 16:22:22 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -22,16 +22,16 @@ def checkout(skus):
             three_deal_total = (five_deal_remainder//3)*130
             three_deal_remainder = (five_deal_remainder%3)*sku_map[k]
             total+=(five_deal_total+three_deal_total+three_deal_remainder)
-        elif k=='E':
-            total_B_free=v//2
-            total+=((sku_map[k]*v)-(total_B_free*sku_map['B']))
         elif k=='B':
-            deal_total = (v//2)*45
+            total_B_free=sku_map['E']//2
+            B_left = v-total_B_free
+            deal_total = (B_left//2)*45
             remainder = (v%2)*sku_map[k]
             total+=(deal_total+remainder)
         else:
             total+=sku_map[k]*v
         print(total)
     return total
+
 
 
