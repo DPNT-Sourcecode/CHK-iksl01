ab5b6917844e2545ea570af846f4550bf200b9a7
sbx_user1051 <sbx_user1051@169.254.189.89>
2023-11-02 15:49:22 +0000

    Thu Nov 02 15:49:22 UTC 2023

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,7 +3,7 @@ from lib.solutions.CHK import checkout_solution
 
 class TestCheckout:
     def test_checkout_no_deal(self):
-        assert checkout_solution.checkout('ABCD') == 50+30+20+15
+        assert checkout_solution.checkout('ABCDE') == 50+30+20+15+40
     
     def test_checkout_3A_deal(self):
         assert checkout_solution.checkout('ABCDAACAA') == 130+30+20+15+20+50+50
@@ -17,7 +17,13 @@ class TestCheckout:
     def test_checkout_B_deal(self):
         assert checkout_solution.checkout('ABCBBBD') == 45+45+50+20+15
     
-    def test_checkout_both_deals(self):
+    def test_checkout_E_deal(self):
+        assert checkout_solution.checkout('ABCDEE') == 50+20+15+40+40
+    
+    def test_checkout_B_and_E_deals(self):
+        assert checkout_solution.checkout('ABCDBEE') == 50+45+20+15+15
+    
+    def test_checkout_3A_and_B_deals(self):
         assert checkout_solution.checkout('ABCDAAB') == 130+45+20+15
     
     def test_checkout_empty(self):
@@ -27,3 +33,4 @@ class TestCheckout:
         assert checkout_solution.checkout('ABCDEG') == -1
     
     
+
